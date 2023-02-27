b83577ba9f270a967074607e92088175a1cfc5ff
stephaniesaggal <stersaggal@gmail.com>
2023-02-26 23:23:54 -0300

    melhorando o código

--- index.js
+++ index.js
@@ -5,14 +5,34 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'arquivo não achado'))
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if(erro){
-            trataErro(erro)
-        }
+//async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try{
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
         console.log(texto)
-    })
+    } catch(erro){
+        trataErro(erro)
+    }
 }
+
+
+
+
+//promises são para códigos assíncrono
+//then você passa o parametro e o que espera de resposta
+//catch é o parametro caso o then não ache o que procura
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(texto))
+//         .catch((trataErro))
+// }
+
+
+
 
 pegaArquivo('./arquivos/texto.md')
