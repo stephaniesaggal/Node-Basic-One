6167e342c93760c14c8ea69ed3845a62f0f2be19
stephaniesaggal <stersaggal@gmail.com>
2023-02-26 23:08:44 -0300

    tratamento de erro

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import fs from "fs";
 import chalk from "chalk";
 
-console.log(chalk.blue('olá mundo'));
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'arquivo não achado'))
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro)
+        }
+        console.log(texto)
+    })
+}
+
+pegaArquivo('./arquivos/texto.md')
